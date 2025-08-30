6eae2a6b821e0ff3b318e7685f435471986b2380
flavio-a-galvao <flavioaugusto.galvao@gmail.com>
2025-08-30 18:16:41 -0300

    adicionando o cadastro

---
+++ cadastro.php
@@ -0,0 +1,24 @@
+<?php
+include 'conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO usuarios (nome, senha) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $nome, $senha);
+
+    if ($stmt->execute()) {
+        echo "Usuário cadastrado com sucesso!";
+    } else {
+        echo "Erro: " . $stmt->error;
+    }
+}
+?>
+
+<form method="POST">
+    Nome: <input type="text" name="nome" required><br>
+    Senha: <input type="password" name="senha" required><br>
+    <button type="submit">Cadastrar</button>
+</form>
